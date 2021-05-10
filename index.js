45be7cf643a510776a4413e07c0f5c74ea354f4b
mflem8 <matthewfleming8@gmail.com>
2021-05-10 10:22:57 -0400

    created function to get data from the server

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const endPoint = "http://localhost:3000/api/v1/vinyls"
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchVinyls()
+    getVinyls()
 })
 
-function fetchVinyls() {
+function getVinyls() {
     fetch(endPoint)
     .then(response => response.json())
     .then(vinyls => {
-        console.log(vinyls);
+        vinyls.data.forEach(vinyl => {
+            const vinylMarkup = `
+            <div data-id=${vinyl.id}>
+                <img src=${vinyl.attributes.image_url}
+                height="200" width="200">
+                <h2>${vinyl.attributes.title}</h2>
+                <h3>${vinyl.attributes.artist}</h3>
+                <h4>${vinyl.attributes.genre.name}</h4>
+                <button data-id=${vinyl.id}>Edit</button>
+            </div>
+            <br><br>`;
+
+            document.querySelector(`#vinyl-container`).innerHTML += vinylMarkup
+            
+        })
     })
 }
